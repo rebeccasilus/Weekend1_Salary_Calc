6f64c4bf9e51e822e729bf3d5e58f2e75128dade
Rebecca Silus <rebeccasilus@gmail.com>
2018-11-10 13:18:21 -0600

    delete button clickable

--- scripts/client.js
+++ scripts/client.js
@@ -1,9 +1,9 @@
 $(document).ready(readyNow);
 
-let deleteEmployeeButton = $('<button>Delete Employee</button>');
-deleteEmployeeButton.click (function (){
-    console.log('Deleting Employee');
-});
+// let deleteEmployeeButton = $('<button>Delete Employee</button>');
+// deleteEmployeeButton.click (function (){
+//     console.log('Deleting Employee');
+// });
 
 class Employee {
     constructor(firstName, lastName, id, title, annualSalary) {
@@ -13,19 +13,15 @@ class Employee {
         this.title = title;
         this.annualSalary = annualSalary;
     } // end constructor
-} // end Employee class
-let staff = [];
+} // Employee class
 
-function readyNow() {
-    console.log('in readyNow');
-    $('#addEmployeeButton').on('click', addEmployee);
-} // readyNow
+let staff = [];
 
 function addEmployee() {
     // optional make all fields mandatory
     if( $('#firstNameIn').val() === '' || $('#lastNameIn').val() === '' || $('#idIn').val() === '' || $('#titleIn').val() === '' || $('#annualSalaryIn').val() === ''){
         alert( "All fields must be completed!");
-    }
+    } // empty fields found
     else{
         console.log('in addEmployee');
     // create a new Employee from class
@@ -51,18 +47,32 @@ function addEmployee() {
 } // addEmployee
 
 function deleteEmployee() {
-}
+    console.log('deleting employee');
+} // deleteEmployee
+
+function readyNow() {
+    console.log('ready to go');
+    $('#addEmployeeButton').on('click', addEmployee);
+} // readyNow
 
 function updateStaff(array){
 console.log('in updateStaff');
 // target the table by id
-let table = $('#outputDiv')
+let table = $('#outputDiv');
 // empty table - turning this off keeps the headers from being deleted but makes doubles for each submit
 table.empty();
 // loop through the staff array and display each employee in the table
 for( let index of array ){
-    let displayString = `<tr><td>${index.firstName}</td><td>${index.lastName}</td><td>${index.id}</td><td>${index.title}</td><td>${index.annualSalary}</td><td id="employeeDelete"></td></tr>`
+    let displayString = `<tr>
+    <td>${index.firstName}</td>
+    <td>${index.lastName}</td>
+    <td>${index.id}</td>
+    <td>${index.title}</td>
+    <td>${index.annualSalary}</td>
+    <td><button id="employeeDelete">Delete</button></td></tr>`
     table.append(displayString);
-    deleteEmployeeButton.appendTo('#employeeDelete')
+    //$('#employeeDelete').append(deleteEmployeeButton);
+    //deleteEmployeeButton.appendTo('#employeeDelete');
 } // end for
-} // end updateStaff
+    $('#employeeDelete').on('click', deleteEmployee);
+} // updateStaff
