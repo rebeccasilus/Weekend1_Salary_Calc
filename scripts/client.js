65ed8dd6fe5c0bed471ae2d0213a57262ed1321e
Rebecca Silus <rebeccasilus@gmail.com>
2018-11-10 19:23:43 -0600

    deletes tables

--- scripts/client.js
+++ scripts/client.js
@@ -1,10 +1,5 @@
 $(document).ready(readyNow);
 
-// let deleteEmployeeButton = $('<button>Delete Employee</button>');
-// deleteEmployeeButton.click (function (){
-//     console.log('Deleting Employee');
-// });
-
 class Employee {
     constructor(firstName, lastName, id, title, annualSalary) {
         this.firstName = firstName;
@@ -20,68 +15,66 @@ let staff = [];
 
 function addEmployee() {
     // optional make all fields mandatory
-    if( $('#firstNameIn').val() === '' || $('#lastNameIn').val() === '' || $('#idIn').val() === '' || $('#titleIn').val() === '' || $('#annualSalaryIn').val() === ''){
-        alert( "All fields must be completed!");
+    if ($('#firstNameIn').val() === '' || $('#lastNameIn').val() === '' || $('#idIn').val() === '' || $('#titleIn').val() === '' || $('#annualSalaryIn').val() === '') {
+        alert("All fields must be completed!");
     } // empty fields found
-    else{
+    else {
         console.log('in addEmployee');
-    // create a new Employee from class
-    // use inputs entered in DOM
-    const tempEmployee = new Employee(
-        $('#firstNameIn').val(),
-        $('#lastNameIn').val(),
-        $('#idIn').val(),
-        $('#titleIn').val(),
-        $('#annualSalaryIn').val(),
+        // create a new Employee from class
+        // use inputs entered in DOM
+        const tempEmployee = new Employee(
+            $('#firstNameIn').val(),
+            $('#lastNameIn').val(),
+            $('#idIn').val(),
+            $('#titleIn').val(),
+            $('#annualSalaryIn').val(),
         );
-    console.log('adding', tempEmployee);
-    // push newEmployee into array
-    staff.push(tempEmployee);
-    //empty the inputs
-    $('#firstNameIn').val('');
-    $('#lastNameIn').val(''),
-    $('#idIn').val(''),
-    $('#titleIn').val(''),
-    $('#annualSalaryIn').val(''),
-    updateStaff (staff);
+        console.log('adding', tempEmployee);
+        // push newEmployee into array
+        staff.push(tempEmployee);
+        //empty the inputs
+        $('#firstNameIn').val('');
+        $('#lastNameIn').val(''),
+            $('#idIn').val(''),
+            $('#titleIn').val(''),
+            $('#annualSalaryIn').val(''),
+            updateStaff(staff);
     } // no empty fields
 } // addEmployee
 
-function deleteEmployee() {
-    console.log('deleting employee');
-} // deleteEmployee
 
 function readyNow() {
     console.log('ready to go');
     $('#addEmployeeButton').on('click', addEmployee);
-    $('#employeeDelete').on('click', deleteEmployee);
+
 } // readyNow
 
 /////////////////////////////////////////////////////////
 function addClickers() {
-    $('.employeeDelete').on('click', deleteEmployee);
+
 } // click listeners added later
 /////////////////////////////////////////////////////////
 
-function updateStaff(array){
-console.log('in updateStaff');
-// target the table by id
-let table = $('#outputDiv');
-// empty table - turning this off keeps the headers from being deleted but makes doubles for each submit
-table.empty();
-// loop through the staff array and display each employee in the table
-for( let index of array ){
-    let displayString = `<tr>
+function updateStaff(array) {
+    console.log('in updateStaff');
+    // target the table by id
+    let table = $('#outputDiv');
+    // empty table - turning this off keeps the headers from being deleted but makes doubles for each submit
+    table.empty();
+    // loop through the staff array and display each employee in the table
+    for (let index of array) {
+        let displayString = `<tr id="${index.id}">
     <td>${index.firstName}</td>
     <td>${index.lastName}</td>
     <td>${index.id}</td>
     <td>${index.title}</td>
     <td>${index.annualSalary}</td>
     <td>${index.monthsIncome}</td>
-    <td><button class="employeeDelete">Delete</button></td></tr>`
-    table.append(displayString);
-     addClickers();
-
-} // end for
- 
+    <td><button id="${index.id}button">Delete</button></td></tr>`
+        table.append(displayString);
+        $(`#${index.id}button`).on('click', function () {
+            console.log(index.id);
+            $(`#${index.id}`).remove();
+        });
+    } // end for
 } // updateStaff
